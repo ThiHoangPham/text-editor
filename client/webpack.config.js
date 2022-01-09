12a5a9250db649d9f1e1e8028ffd131435f455ae
Adam Pham <thaihoangpham2008@gmail.com>
2022-01-10 02:24:18 +0800

    added  WebpackPwaManifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,6 +19,23 @@ module.exports = () => {
         template: "./index.html",
         title: "J.A.T.E",
       }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        description: "Takes notes with JavaScript syntax highlighting!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
