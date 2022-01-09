66149202182aa258e0aa367a5da511f22e9491d9
Adam Pham <thaihoangpham2008@gmail.com>
2022-01-10 02:28:09 +0800

    add module rules, finished webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,10 +10,12 @@ module.exports = () => {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+
     plugins: [
       new HtmlWebpackPlugin({
         template: "./index.html",
@@ -44,7 +46,28 @@ module.exports = () => {
 
     module: {
       rules: [
-
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
