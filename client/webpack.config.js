c82933b801b4af759ef3552d96738cc501a39748
Adam Pham <thaihoangpham2008@gmail.com>
2022-01-10 02:26:03 +0800

    added injectManifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,6 +19,10 @@ module.exports = () => {
         template: "./index.html",
         title: "J.A.T.E",
       }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
